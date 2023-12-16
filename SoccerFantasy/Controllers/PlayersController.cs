842758da58db473eecc841e9833e23ed1d094e9b
modybon <mohamedfahmy3020@gmail.com>
2023-12-15 21:43:42 -0500

    Fixed player replacment in fantasy team.

--- SoccerFantasy/Controllers/PlayersController.cs
+++ SoccerFantasy/Controllers/PlayersController.cs
@@ -16,7 +16,7 @@ namespace SoccerFantasy.Controllers
 
         public ActionResult getPlayersByPosition(string position)
         {
-            
+            var c = CurrentUser.Instance.fantasyTeam.players;
             var players = dataContext.players
                 .Where(p => p.position.Contains(position))
                 .AsEnumerable()
@@ -33,13 +33,37 @@ namespace SoccerFantasy.Controllers
             FantasyTeam fantasyTeam = dataContext.fantasyTeams
                 .Include(ft=> ft.players)
                 .First(ft=> ft.fantasyTeamId == fTeamId);
-            if (!fantasyTeam.players.Contains(player))
-            {
-
-            }
             fantasyTeam.players.Add(player);
             CurrentUser.Instance.fantasyTeam = fantasyTeam;
             dataContext.SaveChanges();
+        }
+        public void replacePlayerInFantasyTeam(string currentPlayerId,string fantasyTeamId, string newPlayerId)
+        {
+            using (var transaction = dataContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    Guid cpId = Guid.Parse(currentPlayerId);
+                    Guid npId = Guid.Parse(newPlayerId);
+                    Guid fTeamId = Guid.Parse(fantasyTeamId);
+                    Player currentPlayer = dataContext.players.First(p => p.playerId == cpId);
+                    Player newPlayer = dataContext.players.First(p => p.playerId == npId);
+                    var c = CurrentUser.Instance.fantasyTeam.players;
+                    FantasyTeam fantasyTeam = dataContext.fantasyTeams
+                        .Include(ft => ft.players)
+                        .First(ft => ft.fantasyTeamId == fTeamId);
+                    fantasyTeam.players.Remove(currentPlayer);
+                    fantasyTeam.players.Add(newPlayer);
+                    CurrentUser.Instance.fantasyTeam.players = fantasyTeam.players;
+                    dataContext.SaveChanges();
+                    transaction.Commit();
+                }catch(Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                    transaction.Rollback();
+                }
+
+            }
         }
     }
 }
